ceb682c7d90e631c199f89dcdfecf748882ca959
Mathews-Velez <velezmathew@gmail.com>
2023-02-19 21:29:52 -0500

    got working poc of a loading bar

--- DSN terminal.md
+++ DSN terminal.md
@@ -3,4 +3,6 @@
 - the controller will behave similar to how a compiler behaves.
 - the controller will be responsible for catching typos and and syntax errors but will not be able to catch run time errors until it is sent to the rover and we get a response from the rover after it recompiles the code after it receives it. 
 - the game lore will explain that there is a size limit to the amount of information that can be sent to the rover across the given distance so to save space it's better to send the code uncompiled and then compiled and ran on the rover . After that we will receive whether or not the code compiled successfully on the rover or not.
-- when the terminal is first open we should have 3 possible menus;(1) directly link to the rover and send it commands with rover.[action] type instructions.(2) script  maker (3) diagnostics check
+- when the terminal is first open we should have 3 possible menus;(1) directly link to the rover and send it commands with rover.[action] type instructions.
+(2) script  maker 
+(3) diagnostics check
---
+++ conceptTests/DsnTerminal.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int centerOutput(std::string input)
+{
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    // printf ("lines %d\n", w.ws_row);
+    // printf ("columns %d\n", w.ws_col);
+    char padding[]{};
+    // get the length of the input provided
+    int len = input.size() / 2;
+
+    int left_padding = w.ws_col / 2 - len;
+    std::string tape = "";
+    for (int i = 0; i < left_padding; i++)
+    {
+        tape += " ";
+    }
+    std::cout << tape;
+    std::cout << input;
+    printf("\n");
+    return 0;
+}
+
+void loadingBar()
+{
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    int screen_width = w.ws_col-3;
+    std::string progress_arrow = "[=>";
+    for (int i = 0; i < screen_width; i++)
+    {
+        usleep(15000);
+        system("clear");
+        progress_arrow.pop_back();
+        progress_arrow.pop_back();
+        progress_arrow += "=>]";
+        printf("\n\n\n\n\n\n\n\n\n");
+        centerOutput("Loading");
+        printf("\n\n\n\n");
+        std::cout << progress_arrow;
+        printf("\n");
+    }
+}
+
+int main()
+{
+    // while (true)
+    // {
+    char response{};
+    printf("\nWould like to establish a link to the NASA Deep Space Network?(y/n)\n");
+    std::cin >> response;
+    if (response == 'y')
+    {
+        loadingBar();
+        return false;
+    }
+    // printf("Establishing a connection....\n");
+    // }
+    return 0;
+}
