e6be85c771b8d9fea11b981ea5d054b40cf03756
Mathews-Velez <velezmathew@gmail.com>
2023-02-27 21:55:11 -0500

    added top padding for text

--- conceptTests/DsnTerminal.cpp
+++ conceptTests/DsnTerminal.cpp
@@ -9,46 +9,76 @@ int centerOutput(std::string input)
 {
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    // printf ("lines %d\n", w.ws_row);
-    // printf ("columns %d\n", w.ws_col);
-    char padding[]{};
     // get the length of the input provided
     int len = input.size() / 2;
 
+    int screen_width = w.ws_col;
+    int screen_height = w.ws_row;
+
+    // adds the proper amount of padding for input to be centered
     int left_padding = w.ws_col / 2 - len;
     std::string tape = "";
     for (int i = 0; i < left_padding; i++)
     {
         tape += " ";
     }
-    std::cout << tape;
-    std::cout << input;
+    tape += input;
+
+    // adds enough newlines before the strings needs to be added to center it vertically
+    int top_padding = screen_height / 2 - 1;
+    std::string roof = "";
+    for (int i = 0; i < top_padding; i++)
+    {
+        roof += "\n";
+    }
+    roof += tape;
+    system("clear");
+    std::cout << roof;
     printf("\n");
     return 0;
 }
 
 void loadingBar()
 {
+    printf("\e[?25l");
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    int screen_width = w.ws_col-3;
+    int screen_width = w.ws_col;
     std::string progress_arrow = "[=>";
-    //generate the empty progress bar
-    
-    for (int i = 0; i < screen_width; i++)
+    std::string progress_bar = "[=";
+
+    // generate the empty progress bar
+    for (int i = 0; i < screen_width - 2; i++)
     {
-        printf("\e[?25l");
-        printf("\n\n\n\n\n\n\n\n\n");
+        progress_bar += " ";
+    }
+    progress_bar += "]";
+
+    //first progress bar version
+    // for (int i = 0; i < screen_width - 3; i++)
+    // {
+    //     centerOutput("Loading");
+    //     printf("\n\n\n\n");
+    //     std::cout << progress_arrow;
+    //     printf("\n");
+    //     usleep(15000);
+    //     system("clear");
+    //     progress_arrow.pop_back();
+    //     progress_arrow.pop_back();
+    //     progress_arrow += "=>]";
+    // }
+
+    for (int i = 1; i < progress_bar.size() - 1; i++){
         centerOutput("Loading");
         printf("\n\n\n\n");
-        std::cout << progress_arrow;
+        progress_bar[i] = '=>';
+        std::cout << progress_bar;
         printf("\n");
         usleep(15000);
         system("clear");
-        progress_arrow.pop_back();
-        progress_arrow.pop_back();
-        progress_arrow += "=>]";
+        progress_bar.pop_back();
     }
+
     printf("\e[?25h");
 }
 
